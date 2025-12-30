37c437787ab153208f3fcdeef8a92e7d34277c6e
pnini <p0548515152@gmail.com>
2025-12-31 00:24:20 +0200

    add function crud

--- project/DotNet2026_5648_7381/DalList/ProductsImplementation.cs
+++ project/DotNet2026_5648_7381/DalList/ProductsImplementation.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DO;
+using DalApi;
 
-namespace Dal
+
+namespace Dal;
+internal class ProductsImplementation : Iproducts
 {
-    internal class ProductsImplementation
+    public int Create(Products item)
     {
+        int newid = DataSource.Products.Count > 0 ? DataSource.Products.Max(c => c.id) + 1 : 1;
+        Products newCustomer = item with { id = newid };
+        DataSource.Products.Add(newCustomer);
+        return newid;
+    }
+    public Products? Read(int id)
+    {
+
+
+        Products x = DataSource.Products.FirstOrDefault(c => c.id == id);
+        if (x == null)
+            throw new Exception($"coustomer {id}  is not exist");
+        return x;
+
+
+    }
+    public List<Products> ReadAll()
+    {
+        return DataSource.Products.ToList();
+    }
+
+    public void Update(Products item)
+    {
+        Delete(item.id);
+        DataSource.Products.Add(item);
+
+    }
+    public void Delete(int id)
+    {
+        int i = DataSource.Products.FindIndex(c => c.id == id);
+        if (i == -1) throw new Exception($"coustomer {id}  is not exist");
+        DataSource.Products.RemoveAt(i);
     }
 }
+
